5f6fb45b474d14d6e199c4d7cb68a2c62e75ed88
gabrieladel <gabi23@live.com.ar>
2023-07-02 16:56:08 -0300

    index

--- example-app/resources/views/index.php
+++ example-app/resources/views/index.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>inicio</title>
 </head>
 <body>
     <h1>hola</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores maxime quis eum dolore magni ut illum minus dicta eos earum? Praesentium magni dolores dicta facilis. Modi, esse iure! Vel, odit!</p>
 </body>
 </html>
