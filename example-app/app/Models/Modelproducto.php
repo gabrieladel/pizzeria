69e95ca0041b9ae0a654ebd93e55133cf86cd293
MONICA-JOFRE <monicagjofre@gmail.com>
2023-07-02 18:34:08 -0300

    clase producto y pedido

---
+++ example-app/app/Models/Modelproducto.php
@@ -0,0 +1,40 @@
+<?php
+
+class Producto {
+    
+    private $producto;
+    private $db;
+
+    public function __construct() {
+        $this->producto = array();
+        $this->db = new PDO('mysql:host=127.0.0.1:3308;dbname=ejemplo_mvc', "root", "");
+    }
+
+    private function setNames() {
+        return $this->db->query("SET NAMES 'utf8'");
+    }
+
+    public function getProductos() {
+
+        self::setNames();
+        $sql = "SELECT id, nombre, precio FROM producto";
+        foreach ($this->db->query($sql) as $res) {
+            $this->producto[] = $res;
+        }
+        return $this->producto;
+    }
+
+    public function setProducto($nombre, $precio) {
+
+        self::setNames();
+        $sql = "INSERT INTO producto(nombre, precio) VALUES ('" . $nombre . "', '" . $precio . "')";
+        $result = $this->db->query($sql);
+
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+?>
---
+++ example-app/app/Models/modelPedido.php
@@ -0,0 +1,40 @@
+<?php
+
+class Pedido {
+    
+    private $pedido;
+    private $db;
+
+    public function __construct() {
+        $this->pedido = array();
+        $this->db = new PDO('mysql:host=127.0.0.1:3308;dbname=ejemplo_mvc', "root", "");
+    }
+
+    private function setNames() {
+        return $this->db->query("SET NAMES 'utf8'");
+    }
+
+    public function getPedidos() {
+
+        self::setNames();
+        $sql = "SELECT id, nombre, precio FROM pedido";
+        foreach ($this->db->query($sql) as $res) {
+            $this->pedido[] = $res;
+        }
+        return $this->pedido;
+    }
+
+    public function setpedido($nombre, $precio) {
+
+        self::setNames();
+        $sql = "INSERT INTO pedido(nombre, precio) VALUES ('" . $nombre . "', '" . $precio . "')";
+        $result = $this->db->query($sql);
+
+        if ($result) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+?>
