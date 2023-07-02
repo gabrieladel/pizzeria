d72df1e50bff379e7acd26c92513e3c10a51611b
gabrieladel <gabi23@live.com.ar>
2023-07-02 20:13:10 -0300

    modificacion Datos

--- example-app/app/Http/Controllers/controllerVendedor.php
+++ example-app/app/Http/Controllers/controllerVendedor.php
@@ -1,6 +1,6 @@
 <?php
     require_once("app/models/modelVendedor.php");
     $vendedores = new Vendedor();
-    $datos = $vendedores->getVendedores();
+    $vende = $vendedores->getVendedores();
     require_once("resources/views/index.php");
 ?>
