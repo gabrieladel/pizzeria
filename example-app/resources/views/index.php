65c8dd6dbf6785f8790e5752d6fcb27abb478c15
gabrieladel <gabi23@live.com.ar>
2023-07-02 18:53:36 -0300

    cambios

--- example-app/resources/views/index.php
+++ example-app/resources/views/index.php
@@ -1,13 +1,48 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>inicio</title>
-</head>
-<body>
-    <h1>hola</h1>
-    <h2>****</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores maxime quis eum dolore magni ut illum minus dicta eos earum? Praesentium magni dolores dicta facilis. Modi, esse iure! Vel, odit!</p>
-</body>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Ejemplo MVC con PHP</title>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <header class="text-center">
+            <strong class="text-danger"><u><h1>Ejemplo MVC con PHP</h1></u></strong>
+                
+                <hr/>
+                <p class="lead">Creamos una base de datos de los servicios <br/>
+                    que podría realizar un taller y <br/>
+                    operamos con ella utilizando el paradigma MVC</p>
+            </header>
+            <div class="col-lg-6 text-center">
+                <hr/>
+                <h3>Listado de servicios</h3>
+                <table class="table">
+                    <tr>
+                        <td><strong>SERVICIO</strong></td>
+                        <td><strong>PRECIO</strong></td>
+                    </tr>
+                    <?php
+                    for ($i = 0; $i < count($datos); $i++) {
+                        ?>
+                        <tr>
+                            <td><?php echo $datos[$i]["nombre"]; ?></td>
+                            <td>$ <?php echo $datos[$i]["precio"]; ?></td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </table>
+                <a href="../index.php"> <i class="fa fa-arrow-circle-left"></i> Volver a la página principal</a>
+                <hr/>
+            </div> 
+            <footer class="col-lg-12 text-danger text-center">
+            <strong><?php echo date("Y"); ?></strong>
+            </footer>
+        </div>
+    </body>
 </html>
