c7b6577f99eeefd8b23533f2db29bca11c055ef4
gabrieladel <gabi23@live.com.ar>
2023-08-21 12:40:11 -0300

    cambios para extender diseño

---
+++ example-app/resources/views/footer.blade.php
@@ -0,0 +1,19 @@
+@yield('footer')
+<footer>
+    <hr>
+    <div class="row text-center" style="color: white">
+        <div>
+            <h6>Seguinos en nuestras redes</h6>
+            <a href="https://www.facebook.com/" target="_blanck" title="Facebook"><i class="fab fa-facebook"
+                    style="color: white"></i></a>
+            <a href="https://www.instagram.com/" target="_blanck" title="Instagram"><i class="fab fa-instagram"
+                    style="color: white"></i></a>
+        </div>
+        <div>
+            <img src="imagenes/icono.png" width="150" height="110">
+            <h4>#Pizzas</h4>
+        </div>
+        <small class="d-block mb-3">© 2023</small>
+    </div>
+    </div>
+</footer>
