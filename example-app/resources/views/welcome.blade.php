930c5ec06f4383e2c99360fe52fccc946c576726
gabrieladel <gabi23@live.com.ar>
2023-08-24 10:54:51 -0300

    cambio de la imagen en el carousel

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -64,13 +64,20 @@
                         <img src="https://images.unsplash.com/photo-1600628421066-f6bda6a7b976?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
                             class="d-block w-100" alt="..." height="400">
                         <div class="carousel-caption d-none d-md-block">
-
                             <h5>Irresistibles</h5>
                             <p>Entra y mira las variedades que tenemos</p>
                             <a href="producto" class="btn btn-light">ver variedades</a>
-
                         </div>
                     </div>
+                    <div class="carousel-item">
+                      <img src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60"
+                          class="d-block w-100" alt="..." height="400">
+                      <div class="carousel-caption d-none d-md-block">
+                          <h5>Irresistibles</h5>
+                          <p>Entra y mira las variedades que tenemos</p>
+                          <a href="producto" class="btn btn-light">ver variedades</a>
+                      </div>
+                  </div>
                 </div>
             </div>
         </section>
