9df0b554de51a55441f9e0869033385d4865b2fa
gabrieladel <gabi23@live.com.ar>
2026-02-12 17:41:58 -0300

    se agrego carrito al header

--- example-app/resources/views/header.blade.php
+++ example-app/resources/views/header.blade.php
@@ -5,31 +5,39 @@
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-          <a class="navbar-brand" href="#">
-            <img src="imagenes/icono.png" width="150" height="110" alt="">#Pizzas
-          </a>
+      <a class="navbar-brand" href="/">
+        <img src="imagenes/icono.png" width="150" height="110" alt="">#Pizzas
+      </a>
     
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
           <li class="nav-item active">
-            <a class="nav-link" href="/">Inicio <span class="sr-only">(current)</span></a>
+            <a class="nav-link" href="/">Inicio</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="producto">Variedades</a>
+            <a class="nav-link" href="/producto">Variedades</a>
           </li>
           <li class="nav-item">
-              <a class="nav-link" href="contacto">Contacto</a>
-            </li>
-        </ul>   <!-- Right Side Of Navbar -->
+            <a class="nav-link" href="/contacto">Contacto</a>
+          </li>
+        </ul>   
         <ul class="navbar-nav ms-auto">
-           <!-- Authentication Links -->
+                <li class="nav-item">
+            <a class="nav-link" href="{{ route('cart.list') }}">
+                <i class="fa fa-shopping-cart"></i> Carrito 
+                @if(\Cart::count() > 0)
+                    <span class="badge badge-danger" style="background-color: red; border-radius: 50%; padding: 2px 6px;">
+                        {{ \Cart::count() }}
+                    </span>
+                @endif
+            </a>
+        </li>
            @guest
                @if (Route::has('login'))
                    <li class="nav-item">
                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                    </li>
                @endif
-
                @if (Route::has('register'))
                    <li class="nav-item">
                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
@@ -37,17 +45,14 @@
                @endif
            @else
                <li class="nav-item dropdown">
-                   <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                   <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                        {{ Auth::user()->name }}
                    </a>
-
                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                        <a class="dropdown-item" href="{{ route('logout') }}"
-                          onclick="event.preventDefault();
-                                        document.getElementById('logout-form').submit();">
+                          onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                            {{ __('Logout') }}
                        </a>
-
                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                            @csrf
                        </form>
@@ -55,5 +60,6 @@
                </li>
            @endguest
        </ul>
+      </div>
   </nav>
-</header> 
+</header>
