e2657ecd864c071fa9d9b5bf583cee570173285f
gabrieladel <gabi23@live.com.ar>
2026-02-14 13:34:55 -0300

    cambios

--- example-app/app/Models/Producto.php
+++ example-app/app/Models/Producto.php
@@ -2,13 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Producto extends Model
 {
-    use HasFactory;
-    protected $table = 'producto';
-    protected $fillable = ['nombre', 'precio'];
-    public $timestamps = false;
+    protected $table = 'productos';
+
+    protected $fillable = [
+        'categoria_id',
+        'nombre',
+        'precio',
+        'imagen',
+        'descripcion'
+    ];
+
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class);
+    }
+
+    public function detalles()
+    {
+        return $this->hasMany(DetallePedido::class);
+    }
 }
