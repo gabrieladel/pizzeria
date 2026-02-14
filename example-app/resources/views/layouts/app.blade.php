36f3a6d90eadca82408b16e3af2f7cb0a36fab29
gabrieladel <gabi23@live.com.ar>
2026-02-14 13:42:55 -0300

    se optimizo el codigo debido a que tenia mucha redundancia

--- example-app/resources/views/layouts/app.blade.php
+++ example-app/resources/views/layouts/app.blade.php
@@ -1,47 +1,35 @@
-@extends('footer')
-
 <!doctype html>
-<html>
+<html lang="es">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>#Pizzas</title>
-    <!-- Fonts -->
+    
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-   {{--  <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
- --}}
-    <!-- Bootstrap CSS v5.2.1 -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-  {{--   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
-    </script> --}}
-    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"
-        integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous">
-    </script>
-</head>
-@yield('content')
-<body>
-    <main class="py-4">
-      @section('contenido')
-
-      @endsection
-    </main>
-    </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     
-  @section('footer')
-  @endsection
-   
-</body>
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js"></script>
 
+    <style>
+        body { background-color: #f8f9fa; }
+        .main-container { min-height: 80vh; }
+    </style>
+</head>
+<body>
+    @include('header')
+    <div id="app">
+        <main class="py-4 main-container">
+            @yield('contenido')
+        </main>
+    </div>
+
+    @include('footer')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 </html>
--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,48 +1,33 @@
 @extends('layouts.app')
 
-@section('content')
+@section('contenido')
 
-@include('header')
-    <section id="inicio">
-        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="https://images.unsplash.com/photo-1584859977999-531c305575b7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHBpenphc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
-                        class="d-block w-100" alt="..." height="400">
-                    <div class="carousel-caption text-white d-none d-md-block">
-                        <h5>Irresistibles</h5>
-                        <p>Entra y mira las variedades que tenemos</p>
-                        <a href="producto" class="btn btn-light">ver variedades</a>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1600628421066-f6bda6a7b976?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBpenphc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60"
-                        class="d-block w-100" alt="..." height="400">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Irresistibles</h5>
-                        <p>Entra y mira las variedades que tenemos</p>
-                        <a href="producto" class="btn btn-light">ver variedades</a>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cGl6emF8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=600&q=60"
-                        class="d-block w-100" alt="..." height="400">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>Irresistibles</h5>
-                        <p>Entra y mira las variedades que tenemos</p>
-                        <a href="producto" class="btn btn-light">ver variedades</a>
-                    </div>
+<section id="inicio">
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://images.unsplash.com/photo-1584859977999-531c305575b7"
+                    class="d-block w-100" height="400">
+                <div class="carousel-caption text-white d-none d-md-block">
+                    <h5>Irresistibles</h5>
+                    <p>Entra y mira las variedades que tenemos</p>
+                    <a href="producto" class="btn btn-light">Ver variedades</a>
                 </div>
             </div>
         </div>
-    </section>
-    <section id="video">
-        <br>
-        <h3 style="text-align: center; color:#ffffff">Asi preparamos tu pizza favorita</h3>
-        <hr>
-        <iframe width="1170" height="394" src="https://www.youtube.com/embed/ywrLSeDVH5U" title="PIZZA" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen></iframe>
-    </section>
-    <section id="Nosotros">
-    </section>
+    </div>
+</section>
+
+<section id="video">
+    <br>
+    <h3 style="text-align: center; color:#000">Así preparamos tu pizza favorita</h3>
+    <hr>
+    <div class="text-center">
+        <iframe width="100%" height="400"
+            src="https://www.youtube.com/embed/ywrLSeDVH5U"
+            allowfullscreen>
+        </iframe>
+    </div>
+</section>
+
+@endsection
