c3bff23d2a99e3eb065b98da6ad5b5651023fcc9
gabrieladel <gabi23@live.com.ar>
2026-02-17 17:56:18 -0300

    cambios

--- example-app/resources/views/header.blade.php
+++ example-app/resources/views/header.blade.php
@@ -1,22 +1,24 @@
-
 <header>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
+      
       <button class="navbar-toggler" type="button" 
-    data-bs-toggle="collapse" 
-    data-bs-target="#navbarTogglerDemo03"
-    aria-controls="navbarTogglerDemo03" 
-    aria-expanded="false" 
-    aria-label="Toggle navigation">
-
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarTogglerDemo03"
+        aria-controls="navbarTogglerDemo03" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <a class="navbar-brand" href="/">
-        <img src="imagenes/icono.png" width="150" height="110" alt="">#Pizzas
+
+      <!-- Logo -->
+      <a class="navbar-brand d-flex align-items-center" href="/">
+        <img src="imagenes/icono.png" width="70" height="70" class="me-2" alt="">
+        <span class="fw-bold fs-4 text-warning">#Pizzas</span>
       </a>
     
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-          <li class="nav-item active">
+          <li class="nav-item">
             <a class="nav-link" href="/">Inicio</a>
           </li>
           <li class="nav-item">
@@ -27,47 +29,58 @@
           </li>
         </ul>   
 
-        
-        <ul class="navbar-nav ms-auto">
-                        <li class="nav-item">
-                    <a class="nav-link" href="{{ route('cart.list') }}">
-                        <i class="fa fa-shopping-cart"></i> Carrito 
-                    @if(\Cart::getTotalQuantity())
-            <span class="badge badge-danger" style="background-color: red; border-radius: 50%; padding: 2px 6px;">
-                {{ \Cart::getTotalQuantity() }}
-            </span>
-        @endif
-    
+        <!-- Lado derecho -->
+        <ul class="navbar-nav ms-auto align-items-center">
+
+          <!-- Carrito -->
+          <li class="nav-item">
+            <a class="nav-link position-relative" href="{{ route('cart.list') }}">
+              
+              <i class="fa fa-shopping-cart fs-5"></i>
+
+              @php
+                $cantidad = method_exists(\Cart::class, 'getTotalQuantity')
+                    ? \Cart::getTotalQuantity()
+                    : \Cart::count();
+              @endphp
+
+              @if($cantidad > 0)
+                <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    {{ $cantidad }}
+                </span>
+              @endif
             </a>
-        </li>
-           @guest
-               @if (Route::has('login'))
-                   <li class="nav-item">
-                       <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                   </li>
-               @endif
-               @if (Route::has('register'))
-                   <li class="nav-item">
-                       <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                   </li>
-               @endif
-           @else
-               <li class="nav-item dropdown">
-                   <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                       {{ Auth::user()->name }}
-                   </a>
-                   <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                       <a class="dropdown-item" href="{{ route('logout') }}"
-                          onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                           {{ __('Logout') }}
-                       </a>
-                       <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                           @csrf
-                       </form>
-                   </div>
-               </li>
-           @endguest
-       </ul>
+          </li>
+
+          @guest
+              @if (Route::has('login'))
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('login') }}">Login</a>
+                  </li>
+              @endif
+              @if (Route::has('register'))
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('register') }}">Registro</a>
+                  </li>
+              @endif
+          @else
+              <li class="nav-item dropdown">
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                      {{ Auth::user()->name }}
+                  </a>
+                  <div class="dropdown-menu dropdown-menu-end">
+                      <a class="dropdown-item" href="{{ route('logout') }}"
+                         onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                          Cerrar sesión
+                      </a>
+                      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                          @csrf
+                      </form>
+                  </div>
+              </li>
+          @endguest
+
+        </ul>
       </div>
   </nav>
 </header>
